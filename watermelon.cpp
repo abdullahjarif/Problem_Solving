4e8ba963188f0db1daa04ea5948daa9ff63f4951
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2024-06-19 20:55:08 +0600

    Update watermelon.cpp

--- watermelon.cpp
+++ watermelon.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int w;
-    cin>>w;
-    if(w>0){
-        if(w>4){cout << "yes" << endl;}
-        else{cout << "no" << endl;}
+    cin >> w;
+    if(w>0)
+    {
+        if(w%2==0 && w>=4)
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
     }
     return 0;
 }
