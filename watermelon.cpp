2c37d670272173eb8f39b66c90d91a784121ef76
Abdullah Jarif <155164388+abdullahjarif@users.noreply.github.com>
2024-06-19 20:43:10 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int w;
+    cin>>w;
+    if(w>0){
+        if(w>4){cout << "yes" << endl;}
+        else{cout << "no" << endl;}
+    }
+    return 0;
+}
